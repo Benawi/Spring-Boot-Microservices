5dad88716b8d9d00a8c9baa24649e8044ae22003
Benawi Alemayehu <habtamu.alemayehu@wku.edu.et>
2024-10-13 15:56:25 +0300

    add order service

--- order-service/src/main/java/service/OrderService.java
+++ order-service/src/main/java/service/OrderService.java
@@ -1,5 +1,24 @@
-package service.Order;
+package service;
+
+import dto.OrderRequest;
+import lombok.RequiredArgsConstructor;
+import model.Order;
+import org.springframework.stereotype.Service;
+import repository.OrderRepository;
+
+import java.util.UUID;
 
 @Service
-public class Service {
+@RequiredArgsConstructor
+public class OrderService {
+    private final OrderRepository orderRepository;
+    public void placeOrder(OrderRequest orderRequest)
+    {
+        Order order=new Order();
+        order.setOrderNumber(UUID.randomUUID().toString());
+        order.setPrice(orderRequest.price());
+        order.setSkuCode(orderRequest.skuCode());
+        order.setQuantity(orderRequest.quantity());
+        orderRepository.save(order);
+    }
 }
