4be78089c701db45815c396f34f378a4ac6d40fa
Benawi Alemayehu <habtamu.alemayehu@wku.edu.et>
2024-10-13 17:13:36 +0300

    refactor the service code

--- order-service/src/main/java/com/teche/microservices/order/service/OrderService.java
+++ order-service/src/main/java/com/teche/microservices/order/service/OrderService.java
@@ -1,11 +1,13 @@
-package service;
+package com.teche.microservices.order.service;
 
-import dto.OrderRequest;
+import com.teche.microservices.order.dto.OrderRequest;
+import com.teche.microservices.order.dto.OrderResponse;
 import lombok.RequiredArgsConstructor;
-import model.Order;
+import com.teche.microservices.order.model.Order;
 import org.springframework.stereotype.Service;
-import repository.OrderRepository;
+import com.teche.microservices.order.repository.OrderRepository;
 
+import java.util.List;
 import java.util.UUID;
 
 @Service
@@ -20,5 +22,11 @@ public class OrderService {
         order.setSkuCode(orderRequest.skuCode());
         order.setQuantity(orderRequest.quantity());
         orderRepository.save(order);
+    }
+
+    public List<OrderResponse> getAllOrders() {
+        return orderRepository.findAll().stream()
+                .map(order -> new OrderResponse(order.getId(),order.getOrderNumber(),order.getSkuCode(),order.getPrice(),order.getQuantity()))
+                .toList();
     }
 }
