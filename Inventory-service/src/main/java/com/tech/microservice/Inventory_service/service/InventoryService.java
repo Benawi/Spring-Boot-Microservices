fc8ee78bdbedce5a00c9c682ed85e7cd073458f5
Benawi Alemayehu <habtamu.alemayehu@wku.edu.et>
2024-10-23 16:26:01 +0300

    add service

--- Inventory-service/src/main/java/com/tech/microservice/Inventory_service/service/InventoryService.java
+++ Inventory-service/src/main/java/com/tech/microservice/Inventory_service/service/InventoryService.java
@@ -1,2 +1,18 @@
-package com.tech.microservice.Inventory_service.service;public class InventoryService {
+package com.tech.microservice.Inventory_service.service;
+
+import com.tech.microservice.Inventory_service.repository.InventoryRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+@RequiredArgsConstructor
+public class InventoryService {
+
+    private final InventoryRepository inventoryRepository;
+
+    @Transactional(readOnly = true)
+    public boolean isInStock(String skuCode, Integer quantity) {
+        return inventoryRepository.existsBySkuCodeAndQuantityIsGreaterThanEqual(skuCode, quantity);
+    }
 }
