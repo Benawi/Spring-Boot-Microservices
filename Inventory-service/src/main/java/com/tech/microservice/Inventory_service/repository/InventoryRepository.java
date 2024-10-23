bb2f8c15babf22d33f2bb8a761a6652c810030e9
Benawi Alemayehu <habtamu.alemayehu@wku.edu.et>
2024-10-23 16:38:09 +0300

    fix bugs

--- Inventory-service/src/main/java/com/tech/microservice/Inventory_service/repository/InventoryRepository.java
+++ Inventory-service/src/main/java/com/tech/microservice/Inventory_service/repository/InventoryRepository.java
@@ -1,2 +1,10 @@
-package com.tech.microservice.Inventory_service.repository;public interface InventoryRepository {
+package com.tech.microservice.Inventory_service.repository;
+
+import com.tech.microservice.Inventory_service.model.Inventory;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface InventoryRepository extends JpaRepository<Inventory, Long> {
+    boolean existsBySkuCodeAndQuantityIsGreaterThanEqual(String sku_code, int quantity);
+
+
 }
---
+++ Inventory-service/src/main/resources/db/migration/V2__add_inventory.sql
@@ -0,0 +1,5 @@
+INSERT INTO tbl_inventory(quantity,sku_code)
+VALUES(12,'dashen02'),
+VALUES(13,'dashen03'),
+VALUES(14,'dashen04'),
+VALUES(15,'dashen05');
