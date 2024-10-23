810fa34febe78029caadc49a0e899f701baec564
Benawi Alemayehu <habtamu.alemayehu@wku.edu.et>
2024-10-23 16:24:52 +0300

    add model

--- Inventory-service/src/main/java/com/tech/microservice/Inventory_service/model/Inventory.java
+++ Inventory-service/src/main/java/com/tech/microservice/Inventory_service/model/Inventory.java
@@ -1,2 +1,23 @@
-package com.tech.microservice.Inventory_service.model;public class Inventory {
+package com.tech.microservice.Inventory_service.model;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Table(name="tbl_inventory")
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+
+public class Inventory {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+    private String sku_code;
+    private Integer quantity;
+
 }
