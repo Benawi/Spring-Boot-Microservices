db86fd441d25f20ce726e18615a7e61b418dc21b
Benawi Alemayehu <habtamu.alemayehu@wku.edu.et>
2024-10-23 16:23:50 +0300

    add controller

--- Inventory-service/src/main/java/com/tech/microservice/Inventory_service/controller/InventoryController.java
+++ Inventory-service/src/main/java/com/tech/microservice/Inventory_service/controller/InventoryController.java
@@ -1,2 +1,18 @@
-package com.tech.microservice.Inventory_service.controller;public class InventoryController {
+package com.tech.microservice.Inventory_service.controller;
+
+import com.tech.microservice.Inventory_service.service.InventoryService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/api/inventory")
+@RequiredArgsConstructor
+public class InventoryController {
+    private final InventoryService inventoryService;
+    @GetMapping
+    @ResponseStatus(HttpStatus.OK)
+    public boolean isInStock(@RequestParam String sku_code, @RequestParam Integer quantity){
+       return inventoryService.isInStock(sku_code,quantity);
+    }
 }
